91211fc951d831ab7eba0dcf6749a749895f8f5c
James Jacobsen <edjcjacobsen@gmail.com>
2024-02-07 00:52:02 -0600

    added logic to push moves into pawn movesets

--- server/routes/gameState.router.js
+++ server/routes/gameState.router.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const gameMoves = [];
 const gameState = [
   ["lr", "ln", "lb", "lq", "lk", "lb", "ln", "lr"],
-  ["lp", "lp", "lp", "lp", "lp", "lp", "lp", "lp"],
+  ["e", "lp", "lp", "lp", "lp", "lp", "lp", "lp"],
+  ["e", "e", "e", "e", "e", "dp", "e", "dp"],
   ["e", "e", "e", "e", "e", "e", "e", "e"],
+  ["lp", "e", "e", "e", "e", "e", "e", "e"],
   ["e", "e", "e", "e", "e", "e", "e", "e"],
-  ["e", "e", "e", "e", "e", "e", "e", "e"],
-  ["e", "e", "e", "e", "e", "e", "e", "e"],
-  ["dp", "dp", "dp", "dp", "dp", "dp", "dp", "dp"],
+  ["dp", "dp", "dp", "dp", "dp", "e", "dp", "e"],
   ["dr", "dn", "db", "dq", "dk", "db", "dn", "dr"],
 ];
 router.get("/", async (req, res) => {
--- server/routes/moveset.router.js
+++ server/routes/moveset.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const SQUARES_PER_SIDE = 8;
 const state = [];
 router.get("/state/:state/color/:color", (req, res) => {
   const playerColor = req.params.color;
@@ -10,21 +11,72 @@ router.get("/state/:state/color/:color", (req, res) => {
   const allPieces = formatPieces();
   const teamPieces = filterTeamPieces(playerColor, allPieces);
   const enemyPieces = filterTeamPieces(enemyColor, allPieces);
-  console.log("teamPieces: ", teamPieces);
-  console.log("enemyPieces: ", enemyPieces);
+  getMoves(teamPieces);
   res.send("eyyy");
 });
 
+function pawnMoves(pawn) {
+  const HOME_ROW = pawn.color == "white" ? 1 : 6;
+  const Y_DIRECTION = pawn.color == "white" ? 1 : -1;
+  const ENEMY_INDICATOR = pawn.color == "white" ? "d" : "l";
+
+  if (
+    pawn.col > 0 &&
+    state[pawn.row + Y_DIRECTION][pawn.col - 1][0] == ENEMY_INDICATOR
+  ) {
+    pawn.moveset.push([pawn.row + Y_DIRECTION, pawn.col - 1]);
+  }
+  if (
+    pawn.col < SQUARES_PER_SIDE - 1 &&
+    state[pawn.row + Y_DIRECTION][pawn.col + 1][0] == ENEMY_INDICATOR
+  ) {
+    pawn.moveset.push([pawn.row + Y_DIRECTION, pawn.col + 1]);
+  }
+
+  if (state[pawn.row + Y_DIRECTION][pawn.col] == "e") {
+    pawn.moveset.push([pawn.row + Y_DIRECTION, pawn.col]);
+    if (
+      pawn.row == HOME_ROW &&
+      state[pawn.row + Y_DIRECTION + Y_DIRECTION][pawn.col] == "e"
+    ) {
+      pawn.moveset.push([pawn.row + Y_DIRECTION + Y_DIRECTION, pawn.col]);
+    }
+  }
+  console.log(pawn.moveset);
+}
+
+function determineMoveType(piece) {
+  if (piece.pieceType == "p") {
+    return pawnMoves(piece);
+  } else if (
+    piece.pieceType == "q" ||
+    piece.pieceType == "r" ||
+    piece.pieceType == "b"
+  ) {
+    // return extenderMoves(piece)
+  } else {
+    // return nonExtenderMoves(piece)
+  }
+}
+
+function getMoves(pieces) {
+  for (let i = 0; i < pieces.length; i++) {
+    determineMoveType(pieces[i]);
+  }
+}
+
 function formatPieces() {
   const formattedPieces = [];
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
+  for (let i = 0; i < SQUARES_PER_SIDE; i++) {
+    for (let j = 0; j < SQUARES_PER_SIDE; j++) {
       if (state[i][j] != "e") {
         const str = state[i][j];
         const color = str[0] == "l" ? "white" : "black";
         const pieceType = str[1];
+        const row = i;
+        const col = j;
         const moveset = [];
-        formattedPieces.push({ pieceType, color, moveset });
+        formattedPieces.push({ pieceType, color, row, col, moveset });
       }
     }
   }
@@ -38,19 +90,12 @@ function filterTeamPieces(color, allPieces) {
 }
 
 function formatState(unformattedState) {
-  for (let i = 0; i < 8; i++) {
-    const row = unformattedState.slice(i * 8, (i + 1) * 8);
+  for (let i = 0; i < SQUARES_PER_SIDE; i++) {
+    const row = unformattedState.slice(
+      i * SQUARES_PER_SIDE,
+      (i + 1) * SQUARES_PER_SIDE
+    );
     state.push(row);
-  }
-}
-
-function determineMoveType(piece) {
-  if (piece.pieceType == "p") {
-    // return pawnMoves(piece)
-  } else if (piece.pieceType == ("q" || "r" || "b")) {
-    // return extenderMoves(piece)
-  } else {
-    // return nonExtenderMoves(piece)
   }
 }
 
